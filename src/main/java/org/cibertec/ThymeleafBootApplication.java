d456201a7ec9340f40093e1905856a09e887f337
Leonardo Pereyra <leonralph3d@gmail.com>
2025-02-22 18:24:39 -0500

    ahora si esta actualizado

---
+++ src/main/java/org/cibertec/ThymeleafBootApplication.java
@@ -0,0 +1,31 @@
+package org.cibertec;
+
+import java.util.List;
+
+import org.cibertec.entity.Area;
+import org.cibertec.service.AreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBootApplication implements CommandLineRunner {
+
+	@Autowired
+	private AreaService areaService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Integer contador = areaService.getTotalAreasInactivas();
+		System.out.println("Total de Areas Inactivas: " + contador);
+
+		List<Area> areaList = areaService.getAreasInactivasProcedure();
+		areaList.forEach(System.out::println);
+	}
+
+}
